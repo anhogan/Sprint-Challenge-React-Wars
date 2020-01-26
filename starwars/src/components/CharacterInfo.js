27fab6a4ec644a225bdeec05b6f7cbffd1715c8e
Amanda Hogan <ahogan@teamworks.com>
2020-01-26 16:24:25 -0500

    Card alignment

--- starwars/src/components/CharacterInfo.js
+++ starwars/src/components/CharacterInfo.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import { Card, CardBody, CardTitle, CardText, CardSubtitle } from 'reactstrap';
+import { Card, CardBody, CardTitle, CardText, CardSubtitle, Container, Row, Col } from 'reactstrap';
 
 const CharacterInfo = (props) => {
 
   return (
-    <div>
-      <Card>
-        <CardBody>
-          <CardTitle>{props.name}</CardTitle>
-          <CardText>Birth Year: {props.birth_year}</CardText>
-          <CardText>Gender: {props.gender}</CardText>
-          <CardSubtitle>Appears In:</CardSubtitle>
-          <CardText>{props.films.map((film) => film.title)}</CardText>
-        </CardBody>
-      </Card>
-    </div>
+    <Container>
+      <Row>
+        <Col md={{ size: 8, order: 2, offset: 2 }}>
+          <Card>
+            <CardBody>
+              <CardTitle>{props.name}</CardTitle>
+              <CardText>Birth Year: {props.birth_year}</CardText>
+              <CardText>Gender: {props.gender}</CardText>
+              <CardSubtitle>Appears In:</CardSubtitle>
+              <CardText>{props.films.map((film) => film.title)}</CardText>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
