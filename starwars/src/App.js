8dac60fe3f5af5f61b35b69d362eacc97b5ad9ef
Amanda Hogan <ahogan@teamworks.com>
2020-01-26 17:12:53 -0500

    Add functionality to buttons

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -9,11 +9,22 @@ const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+  const [object, setObject] = useState([]);
   const [characters, setCharacters] = useState([]);
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  useEffect(() => {
+    axios.get('https://swapi.co/api/people/?page=1')
+      .then(response => {
+        setObject(response.data)
+      })
+      .catch(error => {
+        console.log(error.message)
+      })
+  }, [nextPage, previousPage]);
 
   useEffect(() => {
     axios.get('https://swapi.co/api/people/?page=1')
@@ -26,27 +37,29 @@ const App = () => {
   }, []);
 
   function previousPage() {
-    const previousURL = characters.previous;
+    const previousURL = object.previous;
     console.log(previousURL);
     axios.get(previousURL)
       .then(response => {
+        setObject(response.data)
         setCharacters(response.data.results)
       })
       .catch(error => {
         console.log(error.message)
-      }, [previousPage])
+      })
   };
 
   function nextPage() {
-    const nextURL = characters.next;
+    const nextURL = object.next;
     console.log(nextURL);
     axios.get(nextURL)
       .then(response => {
+        setObject(response.data)
         setCharacters(response.data.results)
       })
       .catch(error => {
         console.log(error.message)
-      }, [nextPage])
+      })
   };
 
   return (
